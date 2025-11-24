45e1ba30b25942ff735ab252ec865df8e7f2511e
594marc-bit <594marc@gmail.com>
2025-11-25 01:39:15 +0800

    12

--- Save0/MultiDronesPlant.py
+++ Save0/MultiDronesPlant.py
@@ -512,5 +512,5 @@ if num_drones() < max_drones():
 	spawn_drone(plant_section_14)
 if num_drones() < max_drones():
 	spawn_drone(plant_section_15)
-#  主线程执行第0部分的种植任务11
+#  主线程执行第0部分的种植任务12
 plant_section_0()
